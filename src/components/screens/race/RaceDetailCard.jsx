67d9beaa09813416a2c97406dc2a0eed13c14934
Ilan Petiot <91156471+IlanHopti@users.noreply.github.com>
2024-03-01 13:33:35 +0100

    fix: races

--- src/components/screens/race/RaceDetailCard.jsx
+++ src/components/screens/race/RaceDetailCard.jsx
@@ -1,70 +1,75 @@
-import React, { useEffect, useState } from 'react'
-import { TouchableOpacity, StyleSheet, Text, Image } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {TouchableOpacity, StyleSheet, Text, Image} from 'react-native';
 import axios from 'axios';
 
-export default function RaceDetailCard({ data }) {
+export default function RaceDetailCard({data}) {
+  const [driverDetails, setDriverDetails] = useState(null);
 
-    const [driverDetails, setDriverDetails] = useState(null);
+  useEffect(() => {
+    getDriverDetails();
+  }, []);
 
-    useEffect(() => {
-        getDriverDetails();
-    }, [])
-
-    const getDriverDetails = async () => {
-        try {
-            const res = await axios.get("https://api.openf1.org/v1/drivers?driver_number=" + data.driver_number);
-            setDriverDetails(res.data[0]);
-        } catch (error) {
-            console.error('ERROR', error);
-        }
+  const getDriverDetails = async () => {
+    try {
+      const res = await axios.get(
+        'https://api.openf1.org/v1/drivers?driver_number=' + data.driver_number,
+      );
+      setDriverDetails(res.data[0]);
+    } catch (error) {
+      console.error('ERROR', error);
     }
+  };
 
-    return (
-        <TouchableOpacity style={styles.card}>
-            <Text style={styles.title}>Position: {data.position}</Text>
-            {driverDetails && (
-                <React.Fragment>
-                    <Text style={styles.subtitle}>Driver Name: {driverDetails.full_name}</Text>
-                    <Text style={styles.text}>Country: {driverDetails.country_code}</Text>
-                    <Text style={styles.text}>Team: {driverDetails.team_name}</Text>
-                    <Image source={{ uri: driverDetails.headshot_url }} style={styles.image} />
-                </React.Fragment>
-            )}
-            <Text style={styles.text}>Date: {data.date}</Text>
-        </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity style={styles.card}>
+      <Text style={styles.title}>Position: {data.position}</Text>
+      {driverDetails && (
+        <React.Fragment>
+          <Text style={styles.subtitle}>
+            Driver Name: {driverDetails.full_name}
+          </Text>
+          <Text style={styles.text}>Country: {driverDetails.country_code}</Text>
+          <Text style={styles.text}>Team: {driverDetails.team_name}</Text>
+          <Image
+            source={{uri: driverDetails.headshot_url}}
+            style={styles.image}
+          />
+        </React.Fragment>
+      )}
+      <Text style={styles.text}>Date: {data.date}</Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    card: {
-        backgroundColor: '#191919',
-        borderRadius: 10,
-        padding: 15,
-        marginVertical: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        
-    },
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 5,
-        color: '#fff',
-    },
-    subtitle: {
-        color: '#888',
-        marginBottom: 5,
-    },
-    image: {
-        width: 100,
-        height: 100,
-        resizeMode: 'contain',
-        marginBottom: 5,
-    },
-    text: {
-        color: '#fff',
-    }
+  card: {
+    backgroundColor: '#191919',
+    borderRadius: 10,
+    padding: 15,
+    marginVertical: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: '#fff',
+  },
+  subtitle: {
+    color: '#888',
+    marginBottom: 5,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+    marginBottom: 5,
+  },
+  text: {
+    color: '#fff',
+  },
 });
